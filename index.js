e7f9be6fecaa5a4f65839d2b77b2bd1b7fdbe0d5
thanhnc1308 <thanhnc138@gmail.com>
2022-03-27 22:58:24 +0700

    add check exists and process multiple images

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ import {
     NEW_S3_HOST,
     OLD_BUCKET,
     NEW_BUCKET,
+    CONCURRENCY_COUNT,
 } from './config.js';
 import { createObject, getObject } from './AWSS3Provider.js';
 import sharp from 'sharp';
@@ -14,21 +15,42 @@ import sharp from 'sharp';
 async function main() {
     await connect();
     // mock.generateMockImageData();
-    const listImages = await getListImages();
-    console.log(listImages.length);
-    for (const image of listImages) {
-        await processImage(image);
+    console.time('reduce_image');
+    while (await checkExistsRawImages()) {
+        const listImages = await getListImages();
+        console.log('listImages.length:', listImages.length);
+        processMultipleTask(listImages);
     }
+    console.timeEnd('reduce_image');
 }
 
 async function connect() {
     await mongoose.connect(MONGODB_CONNECTION_STRING);
 }
 
+async function processSingleTask(listImages) {
+    for (const image of listImages) {
+        await processImage(image);
+    }
+}
+
+async function processMultipleTask(listImages) {
+    if (Array.isArray(listImages) && listImages.length > 0) {
+        const listPromises = listImages.map((image) => processImage(image));
+        await Promise.all(listPromises);
+    }
+}
+
+async function checkExistsRawImages() {
+    return await Image.exists({
+        url: new RegExp(OLD_S3_HOST, 'i'),
+    });
+}
+
 async function getListImages() {
     return await Image.find({
         url: new RegExp(OLD_S3_HOST, 'i'),
-    });
+    }).limit(CONCURRENCY_COUNT);
     // return await Image.find({
     //     isResized: {
     //         $in: [void 0, false],
@@ -43,6 +65,7 @@ async function processImage(image) {
         const options = {
             width: 80,
             height: 100,
+            fit: sharp.fit.outside,
         };
         const resizeImageBuffer = await resizeImage(rawImageBuffer, options);
         const result = await uploadImageToS3(imageName, resizeImageBuffer);
@@ -107,6 +130,7 @@ async function updateNewUrl(image) {
     const newUrl = url.replace(OLD_S3_HOST, NEW_S3_HOST);
     await Image.findByIdAndUpdate(id, {
         url: newUrl,
+        // isResized: true
     });
 }
 
